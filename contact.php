8ac44cde7b5f57da3c224371573c304a7ee333ba
allwinjoseph <54492110+allwinjoseph@users.noreply.github.com>
2025-03-23 11:48:00 +0530

    initial commit

---
+++ contact.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+<!-- MetaTags - Online Meta Tag http://www.onlinemetatag.com -->
+<!-- Create your meta tag and increase your website position on search engines! -->
+
+    <title>Contact Us</title>
+    <META NAME="description" CONTENT="Furniture and Home Decor Show room in Villukuri, Thuckalay">
+<META NAME="keywords" CONTENT="Furniture in villukuri, furniture in thuckalay, furniture in nagercoil, furnitures in kanyakumari, cot, bed, Wooden cupboards, Biro, Tables, Dining Tables, Office Table, Wheel chairs, Kids table, cradle, steel biro, House decors">
+<META NAME="robot" CONTENT="index,follow">
+<META NAME="generator" CONTENT="www.onlinemetatag.com">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Acme|BioRhyme:400,700|Kreon" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rokkitt|Sriracha" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lobster|Merriweather" rel="stylesheet">
+  <link rel="icon" href="images/favicon.png" type="image/png">
+   
+<style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 70%;
+   
+      margin-left: 150px;
+      margin-bottom: 25px;
+      
+  }
+  </style>
+  <script src='https://www.google.com/recaptcha/api.js'></script>
+       
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <!--header starts here-->
+  <div class="container-fluid">
+  <div class="container-fluid">
+<div class="row">
+<div class="col-sm-12 header">
+<img src="images/logo.png" class="img-responsive" alt="">
+</div>
+</div>
+  </div>
+ <!--header ends here-->
+   <!--navbar starts here-->
+  <nav class="navbar navbar-inverse navbar-static-top nav1">
+  <div class="container-fluid">
+  <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      </div>
+       <div class="collapse navbar-collapse" id="myNavbar">
+   <ul class="nav navbar-nav">
+      <li><a href="index.html">Home</a></li>
+      <li ><a href="about.html">About Us</a></li>
+      <li><a href="furniture.html">Furnitures</a></li> 
+      <li><a href="craft.html">Crafts and Antiques</a></li> 
+      <li class="active"><a href="contact.html">Contact Us</a></li>
+    </ul>
+  </div>
+  </div>
+</nav>
+ <!--navbar ends here-->
+<div class="container contacth">
+<div class="row">
+  <form role="form" action="email_script.php" method="post" >
+    <div class="col-lg-6">
+      <div class="well well-sm"><strong><i class="glyphicon glyphicon-ok form-control-feedback"></i> Required Field</strong></div>
+      <div class="form-group">
+        <label for="InputName">Your Name</label>
+        <div class="input-group">
+          <input type="text" class="form-control" name="name" id="InputName" placeholder="Enter Name" required>
+          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+      </div>
+      <div class="form-group">
+        <label for="InputEmail">Your Email</label>
+        <div class="input-group">
+          <input type="email" class="form-control" id="InputEmail" name="email" placeholder="Enter Email" required  >
+          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+      </div>
+      <div class="form-group">
+        <label for="InputMessage">Message</label>
+        <div class="input-group">
+          <textarea name="message" id="InputMessage" class="form-control" rows="5" required></textarea>
+          <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span></div>
+      </div>
+      <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info pull-right">
+    </div>
+    <div class="g-recaptcha" data-sitekey="6LdJFAcUAAAAADADiJzw1AA7ZxcN_KpWjKVZHi9Z"></div>
+  </form>
+ 
+  <hr class="featurette-divider hidden-lg">
+  <div class="col-lg-5 col-md-push-1">
+    <address>
+    <u><h3>Office Location</h3></u>
+    <p class="lead"><a href="http://www.subhasakthifurniture.in/">Subha Sakthi Foam Products and Furnitures</a><br>
+9-20B TVM Main Road, Villukuri<br>
+     Kanyakumari district.<br>
+     Landline no:04651-258607<br>
+      Mobile no:+91 9487658257, +919443382107</p>
+    </address>
+  </div>
+</div>
+
+</div>
+<div class="footer">
+<div class="col-sm-4 col-xs-12">
+<ul class="fmenu">
+<li><a href="about.html">About Us</a></li>
+<li><a href="furniture.html">Furnitures</a></li>
+<li><a href="craft.html">Crafts</a></li>
+<li><a href="craft.html">Antiques</a></li>
+</ul>
+</div>
+
+<div class="col-md-4 col-lg-4">
+<div class="fcon">
+	
+	<p class="fcon2" style="padding-left:100px;"><span class="fcon1">Contact Us</span> <br>Subha Shakthi Foam products and Furniture<br>Trivandrum Main Road<br>Villukuri, Thuckalay.<br>Mobile no:+91 9487658257, +91 9443382107</p>
+	</div>
+</div>
+
+<div class="col-sm-3 col-md-4">
+
+<div class="fright">
+<p><span class="fright1">Our Services</span><br>We help customers design their dream home by providing them with a fantastic line of high quality furniture to help them transform their living spaces from the ordinary into the extraordinary, in no time.</p>
+
+</div>
+</div>
+
+<div class="ftext">
+<center><p>Copyright&copy;2016 <a href="http://www.subhasakthifurniture.in">Subha Sakthi foam products</a>.Designed by <a href="http://www.allevainfotech.com">Alleva Infotech</a></p></center>
+</div>
+</div>
+</div>
+<script>
+    wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
+  </script>
+  <script>
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+      interval: 10000
+  })
+});
+
+  </script>
+  <script>
+  $('.carousel').carousel({
+    interval: 2000
+  })
+</script>
+</body>
+
+</html>
